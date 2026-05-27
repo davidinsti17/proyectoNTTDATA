878b00bff693c2ab56fdcf148edd5baf2031222b
David Estepa Cabrera <destcab2912@g.educaand.es>
2026-05-27 13:26:32 +0200

    error en nombres cambiado

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO author (name, birth_year) VALUES ('Jesús Baptista', 1892);
+INSERT INTO author (name, birth_year) VALUES ('Jesus Baptista', 1892);
 INSERT INTO author (name, birth_year) VALUES ('David Estepa', 1975);
 INSERT INTO author (name, birth_year) VALUES ('Luis Mascaros', 1948);
 INSERT INTO author (name, birth_year) VALUES ('Teodor Oancea', 1948);
 
-INSERT INTO book (title, published_year, author_id) VALUES ('El señor de los anillos', 1954, 1);
+INSERT INTO book (title, published_year, author_id) VALUES ('El rey de los anillos', 1954, 1);
 INSERT INTO book (title, published_year, author_id) VALUES ('Las dos torres', 1954, 1);
 INSERT INTO book (title, published_year, author_id) VALUES ('El regreso del rey', 1955, 1);
 
@@ -13,4 +13,4 @@ INSERT INTO book (title, published_year, author_id) VALUES ('La guerra de reyes'
 INSERT INTO book (title, published_year, author_id) VALUES ('Juego de tronos', 1996, 3);
 
 
-INSERT INTO book (title, published_year, author_id) VALUES ('Hábitos Atómicos', 2000, 4);
+INSERT INTO book (title, published_year, author_id) VALUES ('Habitos Atomicos', 2000, 4);
--- backend/target/classes/data.sql
+++ backend/target/classes/data.sql
@@ -1,12 +1,16 @@
-INSERT INTO author (name, birth_year) VALUES ('J.R.R. Tolkien', 1892);
-INSERT INTO author (name, birth_year) VALUES ('Brandon Sanderson', 1975);
-INSERT INTO author (name, birth_year) VALUES ('George R.R. Martin', 1948);
+INSERT INTO author (name, birth_year) VALUES ('Jesús Baptista', 1892);
+INSERT INTO author (name, birth_year) VALUES ('David Estepa', 1975);
+INSERT INTO author (name, birth_year) VALUES ('Luis Mascaros', 1948);
+INSERT INTO author (name, birth_year) VALUES ('Teodor Oancea', 1948);
 
-INSERT INTO book (title, published_year, author_id) VALUES ('The Fellowship of the Ring', 1954, 1);
-INSERT INTO book (title, published_year, author_id) VALUES ('The Two Towers', 1954, 1);
-INSERT INTO book (title, published_year, author_id) VALUES ('The Return of the King', 1955, 1);
+INSERT INTO book (title, published_year, author_id) VALUES ('El señor de los anillos', 1954, 1);
+INSERT INTO book (title, published_year, author_id) VALUES ('Las dos torres', 1954, 1);
+INSERT INTO book (title, published_year, author_id) VALUES ('El regreso del rey', 1955, 1);
 
-INSERT INTO book (title, published_year, author_id) VALUES ('Mistborn: The Final Empire', 2006, 2);
-INSERT INTO book (title, published_year, author_id) VALUES ('The Way of Kings', 2010, 2);
+INSERT INTO book (title, published_year, author_id) VALUES ('El Imperio final', 2006, 2);
+INSERT INTO book (title, published_year, author_id) VALUES ('La guerra de reyes', 2010, 2);
 
-INSERT INTO book (title, published_year, author_id) VALUES ('A Game of Thrones', 1996, 3);
+INSERT INTO book (title, published_year, author_id) VALUES ('Juego de tronos', 1996, 3);
+
+
+INSERT INTO book (title, published_year, author_id) VALUES ('Hábitos Atómicos', 2000, 4);
